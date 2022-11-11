9677304be6b227135302be07e2f583427d7c7123
Mmesoma Nwachukwu (nwachukwum) <nwachukwum@coventry.ac.uk>
2022-11-11 07:18:28 +0000

    Update alternate_response.py

--- alternate_response.py
+++ alternate_response.py
@@ -5,7 +5,7 @@ def alternate_response():
     alternates = [
         "My apologies I didn't catch that.",
         "I would appreciate if you could be more descriptive.",
-        "Please try asking another question if I can't yet respond to that one.",
+        "Please try asking another question, I can't yet respond to that one.",
         "It seems like you wrote something that I don't fully grasp.",
         "Would you mind rephrasing it for me?"
     ]
