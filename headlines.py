74dda26ff7bc1f112165278993b9e917fd1d1069
Mmesoma Nwachukwu (nwachukwum) <nwachukwum@coventry.ac.uk>
2022-11-18 09:12:48 +0000

    Update headlines.py

--- headlines.py
+++ headlines.py
@@ -1,23 +1,38 @@
 import requests
+API_KEY = "1a210072c96c49c59719a370d0fbd69b"
 
+specific_country = "us"
 specific_word = "apple"
 specific_publisher = "techcrunch"
+specific_sorting = "popularity"
 
-british_headlines_url = "https://newsapi.org/v2/top-headlines?country=gb&apiKey=1a210072c96c49c59719a370d0fbd69b"
+british_headlines_url = "https://newsapi.org/v2/top-headlines?country=gb&apiKey=" + API_KEY
 
-specific_word_url = "https://newsapi.org/v2/everything?q="+specific_word+\
-                    "&from=2022-11-15&to=2022-11-15&sortBy=popularity&apiKey=1a210072c96c49c59719a370d0fbd69b"
+specific_word_url = "https://newsapi.org/v2/everything?q=" + specific_word +\
+                    "&from=2022-11-15&to=2022-11-15&sortBy=popularity&language=en&apiKey=" + API_KEY
 
-specific_publisher_url = "https://newsapi.org/v2/everything?domains="+specific_publisher+\
-                         ".com&apiKey=1a210072c96c49c59719a370d0fbd69b"
+specific_publisher_url = "https://newsapi.org/v2/everything?domains=" + specific_publisher +".com&apiKey=" + API_KEY
+
+specific_country_url = "https://newsapi.org/v2/top-headlines?country=" + specific_country +"&apiKey=" + API_KEY
+
+custom_url = "https://newsapi.org/v2/everything?domains="+specific_publisher+".com&q=" + specific_word +\
+                    "&from=2022-11-15&to=2022-11-15&sortBy="+specific_sorting+"&apiKey=" + API_KEY
 
 
-response = requests.get(british_headlines_url)
-data = response.json()
+custom_response = requests.get(custom_url)
+custom_data = custom_response.json()
+british_response = requests.get(british_headlines_url)
+british_data = british_response.json()
+country_response = requests.get(specific_country_url)
+country_data = country_response.json()
 specific_response = requests.get(specific_word_url)
 specific_data = specific_response.json()
 publisher_response = requests.get(specific_publisher_url)
 publisher_data = publisher_response.json()
+
+
+def custom():
+    return custom_data.get("articles")
 
 
 def specific_words():
@@ -29,36 +44,53 @@ def publisher():
 
 
 def articles_british():
-    return data.get("articles")
+    return british_data.get("articles")
 
 
 def top_ten_british():
     print("TOP 10 HEADLINES IN ENGLAND")
     for i in range(10):
-        top = data["articles"][i]["title"]
-        print(str(i+1),".", top)
-    return
+        top = british_data["articles"][i]["title"]
+        print(str(i+1),".", top,"\n")
+    return "\n"
+
+
+def top_ten_country():
+    print(f"TOP 10 HEADLINES IN THE {specific_country.upper()}")
+    for c in range(10):
+        top = country_data["articles"][c]["title"]
+        print(str(c+1),".", top,"\n")
+    return "\n"
 
 
 def titles_british():
     for t in articles_british():
-        print(t["title"])
+        print(t["title"],"\n")
+    return "\n"
 
 
 def titles_specific():
-    for s in specific_words():
-        print(s["title"])
+    for w in specific_words():
+        print(w["title"],"\n")
+    return "\n"
 
 
 def titles_publisher():
     for p in publisher():
-        print(p["title"])
+        print(p["title"],"\n")
+    return "\n"
 
-"""
-print(titles_specific())
-print(articles_british())
-print(top_ten_british())
-print(titles_british())
-"""
-print(titles_publisher())
 
+def titles_custom():
+    for cu in custom():
+        print(cu["title"],"\n")
+    return "\n"
+
+
+
+#print(titles_specific())
+#print(top_ten_british())
+#print(titles_british())
+#print(titles_publisher())
+#print(top_ten_country())
+#print(titles_custom())
